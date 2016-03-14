dd3490e766bffee478ccbfc6fdf4f817d692c895
chenyongpeng-1120132200 <chen9121@foxmail.com>
2016-03-14 09:43:01 +0800

    add some domain/bean

---
+++ source/humanresource/src/main/java/com/shandong/human/resource/domain/User.java
@@ -0,0 +1,45 @@
+package com.shandong.human.resource.domain;
+
+/**
+ * Author: chenyongpeng <chen9121@foxmail.com>
+ * Date: 2016/3/14 9:34
+ */
+
+public class User {
+    private int id;            //主键
+    private String username;  //用户名
+    private String password;  //MD5加密密码
+    private int type;         //1:省用户，2:企业用户
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
---
+++ source/humanresource/src/main/java/com/shandong/human/resource/domain/UserRole.java
@@ -0,0 +1,36 @@
+package com.shandong.human.resource.domain;
+
+/**
+ * Author: chenyongpeng <chen9121@foxmail.com>
+ * Date: 2016/3/14 9:37
+ */
+
+public class UserRole {
+    private int id;        //主键
+    private int user_id;  //用户id
+    private int role_id;  //角色id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+}
