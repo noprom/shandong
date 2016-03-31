bb803abbe8c9055b3a3966e94c2e8146ce57343a
noprom <noprom@nopromdeMacBook-Pro.local>
2016-03-31 17:59:55 +0800

    优化代码格式｜注释

--- source/humanresource/src/main/java/com/shandong/human/resource/controller/home/company/CompanyController.java
+++ source/humanresource/src/main/java/com/shandong/human/resource/controller/home/company/CompanyController.java
@@ -25,34 +25,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 公司备案主要的控制类
+ *
  * Author: chenyongpeng <chen9121@foxmail.com>
  * Date: 2016/3/14 10:11
+ * Modified: noprom <tyee.noprom@qq.com>
  */
 @Controller
 public class CompanyController {
 
     // 静态资源前缀
     public static final String STATIC_PREFIX = "human-resource/home/company";
+
     @Autowired
     private AreaService areaService;
+
     @Autowired
     private CompanyService companyService;
 
-
-    //获取所有的城市
-    //获取数据显示页面
-    @RequestMapping(value = "/home/company/add",method = RequestMethod.GET)
+    /**
+     * 获取所有的城市
+     * 获取数据显示页面
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/home/company/add", method = RequestMethod.GET)
     public String getProvince(Model model) {
         ArrayList<Area> list = areaService.getAllCity();
         //刚开始打开页面时显示济南市的所有县
-        ArrayList<Area> listCity=areaService.getAllAreaById(170);
+        ArrayList<Area> listCity = areaService.getAllAreaById(170);
         model.addAttribute("editResult", list);
         model.addAttribute("editResultCity", listCity);
         return STATIC_PREFIX + "/add";
     }
 
-    //获取所有的地区
-    //获取数据显示页面
+    /**
+     * 获取所有的地区
+     * 获取数据显示页面
+     *
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "/home/company/add/{id}", method = RequestMethod.GET)
     @ResponseBody
     public ArrayList<Area> getAllArea(@PathVariable("id") Integer id) {
@@ -62,71 +76,87 @@ public class CompanyController {
         return list;
     }
 
-    //保存企业信息
-    //处理修改请求
-    @RequestMapping(value = "/home/company/add/submit",method = RequestMethod.POST)
+    /**
+     * 保存企业信息
+     * 处理修改请求
+     *
+     * @param company
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/home/company/add/submit", method = RequestMethod.POST)
     @ResponseBody
-    public Map saveMessage(Company company,HttpSession session) {
-        Map map=new HashMap();
-        User user=(User)session.getAttribute(Constant.LOGIN_USER);
-        int id=user.getId();
+    public Map saveMessage(Company company, HttpSession session) {
+        Map map = new HashMap();
+        User user = (User) session.getAttribute(Constant.LOGIN_USER);
+        int id = user.getId();
         //检查数据是否已经插入到数据库中
-        ArrayList<Company> companies=companyService.isNull(id);
-        if(companies.size()>0)
-        {
-            map.put("success","exit");
+        ArrayList<Company> companies = companyService.isNull(id);
+        if (companies.size() > 0) {
+            map.put("success", "exit");
             return map;
         }
         company.setId(id);
         companyService.setCompanyInfo(company);
-        map.put("success","success");
+        map.put("success", "success");
         return map;
     }
 
-    //企业信息修改
-    //获取修改页面
-    @RequestMapping(value = "/home/company/edit",method = RequestMethod.GET)
-    public String getProvinceEdit(Model model,HttpSession session,HttpServletResponse response)throws IOException {
-        User user=(User)session.getAttribute(Constant.LOGIN_USER);
-        int id=user.getId();
-        ArrayList<Company> companies=companyService.isNull(id);
-        if(companies.size()==0)
-        {
+    /**
+     * 企业信息修改
+     * 获取修改页面
+     *
+     * @param model
+     * @param session
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/home/company/edit", method = RequestMethod.GET)
+    public String getProvinceEdit(Model model, HttpSession session, HttpServletResponse response) throws IOException {
+        User user = (User) session.getAttribute(Constant.LOGIN_USER);
+        int id = user.getId();
+        ArrayList<Company> companies = companyService.isNull(id);
+        if (companies.size() == 0) {
             response.sendRedirect("/home/company/add");
             return null;
         }
-        Company company=companyService.getCompanyById(id);
+        Company company = companyService.getCompanyById(id);
         //id是主键，数组只有一个元素
-        Area cityArea=areaService.getById(company.getCity_id());
-        Area areaArea=areaService.getById(company.getArea_id());
+        Area cityArea = areaService.getById(company.getCity_id());
+        Area areaArea = areaService.getById(company.getArea_id());
         ArrayList<Area> listCity = areaService.getAllCity();
-        ArrayList<Area> listArea=areaService.getAllAreaById(company.getCity_id());
+        ArrayList<Area> listArea = areaService.getAllAreaById(company.getCity_id());
         model.addAttribute("listCity", listCity);
         model.addAttribute("listArea", listArea);
-        model.addAttribute("company",company);
-        model.addAttribute("cityArea",cityArea);
-        model.addAttribute("areaArea",areaArea);
+        model.addAttribute("company", company);
+        model.addAttribute("cityArea", cityArea);
+        model.addAttribute("areaArea", areaArea);
         return STATIC_PREFIX + "/edit";
     }
 
-
-    //保存修改信息
+    /**
+     * 保存修改信息
+     *
+     * @param company
+     * @param session
+     * @return
+     */
     @RequestMapping(value = "/home/company/edit", method = RequestMethod.POST)
     @ResponseBody
-    public Map saveInfo(Company company,HttpSession session) {
-        Map map=new HashMap();
-        User user=(User)session.getAttribute(Constant.LOGIN_USER);
-        int id=user.getId();
+    public Map saveInfo(Company company, HttpSession session) {
+        Map map = new HashMap();
+        User user = (User) session.getAttribute(Constant.LOGIN_USER);
+        int id = user.getId();
         //检查数据是否已经插入到数据库中
-        ArrayList<Company> companies=companyService.isNull(id);
-        if(companies.size()==0)
-        {
-            map.put("success","noInfo");
+        ArrayList<Company> companies = companyService.isNull(id);
+        if (companies.size() == 0) {
+            map.put("success", "noInfo");
             return map;
         }
         company.setId(id);
         companyService.updateCompanyInfo(company);
-        map.put("success","success");
+        map.put("success", "success");
         return map;
     }
 }
